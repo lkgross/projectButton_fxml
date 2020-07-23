d20d114accccfca7361deb7e4ea289688ca30f7d
lkgross <profgross@gmail.com>
2020-07-22 21:22:55 -0400

    Tidy up Controller

--- src/main/java/bsu/comp152/Controller.java
+++ src/main/java/bsu/comp152/Controller.java
@@ -2,17 +2,12 @@ package bsu.comp152;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 /**
  * A Controller class
  * See Main.java for details.
  */
-// public class Controller implements Initializable {
 public class Controller {
 
     /* Here @FXML is needed to connect the Label initialized in Main.fxml
@@ -34,7 +29,6 @@ public class Controller {
     public void pressButton(ActionEvent event) {
         // System.out.println("Hello World!");
         intro_message.setText("Thanks for clicking the button!");
-
     }
     /*
     @Override
